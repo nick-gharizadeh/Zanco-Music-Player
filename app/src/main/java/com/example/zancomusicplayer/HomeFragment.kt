ddd4dd6126d51c6ced19fea6c2a280fea801aaee
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-11-28 19:55:07 +0330

    feat/ play music :)

--- app/src/main/java/com/example/zancomusicplayer/HomeFragment.kt
+++ app/src/main/java/com/example/zancomusicplayer/HomeFragment.kt
@@ -4,19 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.zancomusicplayer.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +25,20 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.cvMasalan.setOnClickListener {
+            homeViewModel.playMusic(Utils.SongName.Masalan)
+        }
+        binding.cvBoroSamteAli.setOnClickListener {
+            homeViewModel.playMusic(Utils.SongName.BoroSamteAli)
+        }
+        binding.cvRadePaa.setOnClickListener {
+            homeViewModel.playMusic(Utils.SongName.RadePaa)
+        }
+        binding.cvKhoshamMiad.setOnClickListener {
+            homeViewModel.playMusic(Utils.SongName.KhoshamMiad)
+        }
 
     }
+
+
 }
--- app/src/main/java/com/example/zancomusicplayer/HomeViewModel.kt
+++ app/src/main/java/com/example/zancomusicplayer/HomeViewModel.kt
@@ -1,7 +1,49 @@
 package com.example.zancomusicplayer
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.lifecycle.AndroidViewModel
 
-class HomeViewModel : ViewModel() {
-    
+
+class HomeViewModel(var app: Application) : AndroidViewModel(app) {
+
+    var mMediaPlayer: MediaPlayer = MediaPlayer()
+
+
+    fun playMusic(songName: Utils.SongName) {
+        when (songName) {
+
+            Utils.SongName.Masalan -> {
+                makeMediaPlayerReadyForPlaying(Utils.SongUri.Masalan.uri)
+            }
+            Utils.SongName.BoroSamteAli -> {
+                makeMediaPlayerReadyForPlaying(Utils.SongUri.BoroSamteAli.uri)
+            }
+            Utils.SongName.RadePaa -> {
+                makeMediaPlayerReadyForPlaying(Utils.SongUri.RadePaa.uri)
+            }
+            Utils.SongName.KhoshamMiad -> {
+                makeMediaPlayerReadyForPlaying(Utils.SongUri.KhoshamMiad.uri)
+            }
+        }
+    }
+
+    fun makeMediaPlayerReadyForPlaying(uri: Uri) {
+        if (mMediaPlayer.isPlaying) {
+            mMediaPlayer.stop()
+            mMediaPlayer.reset()
+        }
+        try {
+            mMediaPlayer.setDataSource(
+                app.applicationContext,
+                uri
+            )
+            mMediaPlayer.prepare()
+            mMediaPlayer.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
