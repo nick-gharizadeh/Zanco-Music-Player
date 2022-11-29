768249a9d5dd2384696b9a98124f430c68407791
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-11-29 22:31:08 +0330

    feat/ set cover to image view in control by extension function (matchSrc)

--- app/src/main/java/com/example/zancomusicplayer/HomeFragment.kt
+++ app/src/main/java/com/example/zancomusicplayer/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.zancomusicplayer.databinding.FragmentHomeBinding
@@ -16,8 +15,7 @@ class HomeFragment : Fragment() {
     private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
@@ -27,19 +25,26 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.cvMasalan.setOnClickListener {
             homeViewModel.setVisible(binding.cvControl)
-            homeViewModel.playMusic(Utils.SongName.Masalan)
+            homeViewModel.playMusic(Utils.Song.Masalan)
+            binding.imageViewMasalan.matchSrc(binding.imageViewSong)
         }
         binding.cvBoroSamteAli.setOnClickListener {
             homeViewModel.setVisible(binding.cvControl)
-            homeViewModel.playMusic(Utils.SongName.BoroSamteAli)
+            homeViewModel.playMusic(Utils.Song.BoroSamteAli)
+            binding.imageViewBoroSamteAli.matchSrc(binding.imageViewSong)
+
         }
         binding.cvRadePaa.setOnClickListener {
             homeViewModel.setVisible(binding.cvControl)
-            homeViewModel.playMusic(Utils.SongName.RadePaa)
+            homeViewModel.playMusic(Utils.Song.RadePaa)
+            binding.imageViewRadePaa.matchSrc(binding.imageViewSong)
+
         }
         binding.cvKhoshamMiad.setOnClickListener {
             homeViewModel.setVisible(binding.cvControl)
-            homeViewModel.playMusic(Utils.SongName.KhoshamMiad)
+            homeViewModel.playMusic(Utils.Song.KhoshamMiad)
+            binding.imageViewKhoshamMiad.matchSrc(binding.imageViewSong)
+
         }
         binding.imageViewPause.setOnClickListener {
             homeViewModel.pausePlaying()
@@ -50,7 +55,9 @@ class HomeFragment : Fragment() {
         binding.imageViewStop.setOnClickListener {
             homeViewModel.stopPlaying()
             homeViewModel.setInvisible(binding.cvControl)
-
+        }
+        homeViewModel.songName.observe(viewLifecycleOwner) {
+            binding.textViewSongName.text = it
         }
 
     }
