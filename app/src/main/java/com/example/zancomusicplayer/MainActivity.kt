c4f9bb30a34b0436d8be5524b08cdfbea4475f41
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-11-30 19:18:15 +0330

    feat/ add splash screen

--- app/src/main/java/com/example/zancomusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/zancomusicplayer/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.zancomusicplayer
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import com.example.zancomusicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        supportActionBar?.hide()
+        binding.group.alpha = 0f
+        binding.group.animate().setDuration(1500).alpha(1f).withEndAction {
+            binding.group.visibility = View.GONE
+            supportActionBar?.show()
+            binding.constraintLayout.setBackgroundColor(Color.TRANSPARENT)
+            binding.navHostFragment.visibility = View.VISIBLE
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
     }
 }
